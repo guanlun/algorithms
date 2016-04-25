7a08785eab12a927f83544b316a86f04ec6ec6a7
guanlun <guanlunzhao@gmail.com>
2016-04-25 23:41:14 +0800

    04-25-2016

---
+++ integer-break/Solution.js
@@ -0,0 +1,31 @@
+var integerBreak = function(n) {
+    if (n === 2) {
+        return 1;
+    } else if (n === 3) {
+        return 2;
+    }
+    
+    var lookup = {
+        2: 2,
+        3: 3
+    };
+    
+    for (var i = 4; i <= n; i++) {
+        var max = 0;
+        
+        for (var sub = 2; sub <= n / 2; sub++) {
+            var firstOp = lookup[sub];
+            var secondOp = lookup[i - sub];
+            
+            var result = firstOp * secondOp;
+            
+            if (result > max) {
+                max = result;
+            }
+        }
+        
+        lookup[i] = max;
+    }
+    
+    return lookup[n];
+};
