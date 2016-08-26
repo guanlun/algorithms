98c4b499e12778c489d4adf33edcc7fb8d72dd01
Guanlun Zhao <guanlunzhao@gmail.com>
2016-08-25 21:26:19 -0500

    08-25-2016

--- maximum-subarray/Solution.java
+++ maximum-subarray/Solution.java
@@ -1,64 +1,20 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int i = 0;
-        int len = nums.length;
-        int currSum = 0;
-        int maxSum = 0;
+        int localMax = -Integer.MAX_VALUE;
+        int max = -Integer.MAX_VALUE;
         
-        int positiveSum = 0;
-        int negativeSum = 0;
-        
-        int largestNegativeNum = -Integer.MAX_VALUE;
-        boolean allNegative = true;
-
-        for (i = 0; i < len; i++) {
-            int curr = nums[i];
-            if (curr >= 0) {
-                allNegative = false;
-                break;
+        for (int n : nums) {
+            if (localMax < 0) {
+                localMax = n;
             } else {
-                if (curr > largestNegativeNum) {
-                    largestNegativeNum = curr;
-                }
+                localMax += n;
             }
-        }
-        
-        if (allNegative) {
-            return largestNegativeNum;
-        }
-        
-        while (i < len) {
-            int curr = nums[i];
             
-            if (curr >= 0) {
-                positiveSum = 0;
-
-                while ((i < len) && (nums[i] >= 0)) {
-                    positiveSum += nums[i];
-                    i++;
-                }
-                
-                currSum += positiveSum;
-                
-                if (currSum > maxSum) {
-                    maxSum = currSum;
-                }
-            } else {
-                negativeSum = 0;
-                
-                while ((i < len) && (nums[i] <= 0)) {
-                    negativeSum -= nums[i];
-                    i++;
-                }
-                
-                if (negativeSum > currSum) {
-                    currSum = 0;
-                } else {
-                    currSum -= negativeSum;
-                }
+            if (localMax > max) {
+                max = localMax;
             }
         }
         
-        return maxSum;
+        return max;
     }
 }
