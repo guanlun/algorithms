e8cadd6116e09136bd9a4e9492a0a05a69706540
Guanlun Zhao <guanlunzhao@gmail.com>
2016-11-04 14:31:59 -0500

    11-04-2016

---
+++ valid-perfect-square/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long left = 0;
+        long right = num;
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return (left * left == num);
+    }
+
+}
